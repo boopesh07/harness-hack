7fd3b0fe4d383bd0a652f1db4edb227826807a69
Monika� S <monika.s@publicissapient.com>
2021-06-08 23:35:01 +0530

    changes by monk

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React from 'react';
-import logo from './center.png';
-import './App.css';
-import trophy from './trophy3.png';
+import './App.scss';
 function App() 
 {
   return (
-    <div className="App">
+    <div className="App">  
       <div class="wrapper">
-    <div class="modal modal--congratulations">
+      <div class="modal modal--congratulations">
         <div class="modal-top">
             <img class="modal-icon u-imgResponsive" src="https://dl.dropboxusercontent.com/s/e1t2hhowjcrs7f5/100daysui_100icon.png" alt="Trophy" />
-            <div class="modal-header">Congratulations</div>
-            <div class="modal-subheader">You have successfully completed the 100 Days UI Challenge</div>
+            <div class="modal-header">Congratulations Boopesh!</div>
+            <div class="modal-subheader">You have successfully completed the Challenge</div>
         </div>
         <div class="modal-bottom">
             <button class="modal-btn u-btn u-btn--share">Share</button>
             <button class="modal-btn u-btn u-btn--success">Have a beer</button>
         </div>
+      </div>
     </div>
+
 </div>
-</div>
-      /* <header className="App-header">
-        <div className= "thala">
-        <img src={trophy} className="App-logo" alt="trophy" height="50" width="400"/>
-          <h2> "Hai Thala, Congrats " </h2>
-          </div>
-        <img src={logo} className="App-logo" alt="logo" width="500" height="500"/>
-      </header>
-    </div> */
     )
   }
 
